2438ff19dfab2d894d5c32a6a2df8a2e82f9ecbe
Khorasani95 <hafizmuhammadabdullah29@gmail.com>
2024-06-09 21:46:14 +0500

    JS Ch#1

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("main-heading").innerText =
+  "<span style='color:green;'>We are at house</span>";
+document.getElementById("main-heading").style.color = "yellow";
+("<span style='color:green;'>We are at house</span>");
